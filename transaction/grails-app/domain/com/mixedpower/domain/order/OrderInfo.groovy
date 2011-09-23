df54aaff2e2ad7dae444dd3c6bf2eb402513c6fc
daqing15 <daqing15@gmail.com>
2011-09-24 00:46:29 +0800

    new domain

---
+++ transaction/grails-app/domain/com/mixedpower/domain/order/OrderInfo.groovy
@@ -0,0 +1,12 @@
+package com.mixedpower.domain.order
+
+/**
+ * 业务订单表 tb_order_info
+ * @author daqing chen
+ * @email  daqing15@gmail.com
+ */
+class OrderInfo {
+
+    static constraints = {
+    }
+}
