b887b70ea99adbbeb3e42e568d86910608ffb027
daqing15 <daqing15@gmail.com>
2011-09-23 15:51:15 +0800

    new domain

---
+++ transaction/grails-app/domain/com/mixedpower/domain/FeatureAttribute.groovy
@@ -0,0 +1,12 @@
+package com.mixedpower.domain
+
+/**
+ * 温馨提示信息tb_operation_hint
+ * @author daqing chen
+ * @email  daqing15@gmail.com
+ */
+class FeatureAttribute {
+
+    static constraints = {
+    }
+}
