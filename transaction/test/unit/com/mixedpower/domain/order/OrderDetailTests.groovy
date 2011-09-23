d8cc01893c9bdd7ebba697007ad7e51e272f7772
daqing15 <daqing15@gmail.com>
2011-09-24 01:10:00 +0800

    new domain junit test class

---
+++ transaction/test/unit/com/mixedpower/domain/order/OrderDetailTests.groovy
@@ -0,0 +1,17 @@
+package com.mixedpower.domain.order
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainUnitTestMixin} for usage instructions
+ */
+@TestFor(OrderDetail)
+class OrderDetailTests {
+
+    void testSomething() {
+
+    }
+}
---
+++ transaction/test/unit/com/mixedpower/domain/order/OrderInfoTests.groovy
@@ -0,0 +1,17 @@
+package com.mixedpower.domain.order
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainUnitTestMixin} for usage instructions
+ */
+@TestFor(OrderInfo)
+class OrderInfoTests {
+
+    void testSomething() {
+
+    }
+}
