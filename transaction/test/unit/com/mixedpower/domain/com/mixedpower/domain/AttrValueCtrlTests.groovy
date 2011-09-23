1cd9e1e636ed7f1d180fd6709631c2ab2b7fa896
daqing15 <daqing15@gmail.com>
2011-09-23 17:03:21 +0800

    new test uinit

---
+++ transaction/test/unit/com/mixedpower/domain/com/mixedpower/domain/AttrValueCtrlTests.groovy
@@ -0,0 +1,18 @@
+package com.mixedpower.domain.com.mixedpower.domain
+
+
+
+import grails.test.mixin.*
+
+import com.mixedpower.domain.AttrValueCtrl
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainUnitTestMixin} for usage instructions
+ */
+@TestFor(AttrValueCtrl)
+class AttrValueCtrlTests {
+
+    void testSomething() {
+
+    }
+}
