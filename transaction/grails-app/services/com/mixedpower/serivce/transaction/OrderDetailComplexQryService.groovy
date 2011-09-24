1d679cc92b4a49ea4bfdc862c9750aba0234e667
daqing chen <daqing15@gmail.com>
2011-09-24 20:13:52 +0800

    Service Created

---
+++ transaction/grails-app/services/com/mixedpower/serivce/transaction/OrderDetailComplexQryService.groovy
@@ -0,0 +1,13 @@
+package com.mixedpower.serivce.transaction
+
+/**
+ * Order订单复杂类查询服务类，直接使用GDBC
+ */
+class OrderDetailComplexQryService {
+
+    static transactional = false
+
+    def serviceMethod() {
+
+    }
+}
