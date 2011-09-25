3f37445234700829b0f9cc5de41f952d624adcb1
daqing chen <daqing15@gmail.com>
2011-09-25 18:49:22 +0800

    domain created

--- transaction/grails-app/domain/com/mixedpower/domain/Discount.groovy
+++ transaction/grails-app/domain/com/mixedpower/domain/Discount.groovy
@@ -5,6 +5,15 @@ package com.mixedpower.domain
  */
 class Discount {
 
+    String offerCode
+	String discSort
+	String offerId
+	String discTypeArea
+	String discName
+	String discTypeTime
+	String discCode
+	String sellContent
+
     static constraints = {
     }
 
